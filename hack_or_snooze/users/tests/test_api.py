3e7061eea85ff352e62e8cce6ee357dc8e852c19
Edmond Soun <edmond.soun@gmail.com>
2024-01-16 02:00:48 -0800

    add login tests

--- hack_or_snooze/users/tests/test_api.py
+++ hack_or_snooze/users/tests/test_api.py
@@ -8,23 +8,31 @@ from django.test import TestCase
 # from ninja.errors import AuthenticationError
 
 # from users.models import User
-from users.factories import UserFactory
+from users.factories import UserFactory, FACTORY_USER_PASSWORD
 # from users.schemas import SignupInput, LoginInput
 
 
 class UserAPIAuthTest(TestCase):
     def setUp(self):
-        self.valid_data = {
+        # TODO: investigate why this is outside setup in SIS?
+        self.existing_user = UserFactory()
+
+        self.valid_signup_data = {
             "username": "test",
             "password": "password",
             "first_name": "first",
             "last_name": "last"
         }
 
+        self.valid_login_data = {
+            "username": self.existing_user.username,
+            "password": FACTORY_USER_PASSWORD
+        }
+
     def test_signup_ok(self):
         response = self.client.post(
             '/api/users/signup',
-            data=json.dumps(self.valid_data),
+            data=json.dumps(self.valid_signup_data),
             content_type="application/json"
         )
 
@@ -83,7 +91,7 @@ class UserAPIAuthTest(TestCase):
 
     def test_signup_fail_extra_fields(self):
         invalid_data = {
-            **self.valid_data,
+            **self.valid_signup_data,
             "extra": "extra"
         }
 
@@ -112,11 +120,10 @@ class UserAPIAuthTest(TestCase):
         )
 
     def test_signup_fail_username_exists(self):
-        existing_user = UserFactory()
 
         dupe_user_data = {
-            **self.valid_data,
-            "username": existing_user.username
+            **self.valid_signup_data,
+            "username": self.existing_user.username
         }
 
         response = self.client.post(
@@ -136,7 +143,7 @@ class UserAPIAuthTest(TestCase):
     def test_signup_fail_malformed_username(self):
 
         invalid_data = {
-            **self.valid_data,
+            **self.valid_signup_data,
             "username": "test:username"
         }
 
@@ -164,6 +171,131 @@ class UserAPIAuthTest(TestCase):
                         }
                     }
                 ]
+            }
+        )
+
+    def test_login_ok(self):
+        response = self.client.post(
+            '/api/users/login',
+            data=json.dumps(self.valid_login_data),
+            content_type="application/json"
+        )
+
+        response_json = json.loads(response.content)
+        response_date_joined = response_json["user"]["date_joined"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response_json,
+            {
+                "token": "factoryUser:27bbe49cbca0",
+                "user": {
+                    "stories": [],
+                    "favorites": [],
+                    "username": "factoryUser",
+                    "first_name": "factory_first",
+                    "last_name": "factory_last",
+                    "date_joined": response_date_joined
+                }
+            }
+        )
+
+    def test_login_fail_missing_data(self):
+        invalid_data = {
+            "username": self.existing_user.username,
+        }
+
+        response = self.client.post(
+            '/api/users/login',
+            data=json.dumps(invalid_data),
+            content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 422)
+        self.assertJSONEqual(
+            response.content,
+            {
+                "detail": [
+                    {
+                        "type": "missing",
+                        "loc": [
+                            "body",
+                            "data",
+                            "password"
+                        ],
+                        "msg": "Field required"
+                    }
+                ]
+            }
+        )
+
+    def test_login_fail_extra_fields(self):
+        invalid_data = {
+            **self.valid_login_data,
+            "extra": "extra"
+        }
+
+        response = self.client.post(
+            '/api/users/login',
+            data=json.dumps(invalid_data),
+            content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 422)
+        self.assertJSONEqual(
+            response.content,
+            {
+                "detail": [
+                    {
+                        "type": "extra_forbidden",
+                        "loc": [
+                            "body",
+                            "data",
+                            "extra"
+                        ],
+                        "msg": "Extra inputs are not permitted"
+                    }
+                ]
+            }
+        )
+
+    def test_login_fail_username_does_not_exist(self):
+        invalid_data = {
+            "username": "nonexistent_user",
+            "password": "password"
+        }
+
+        response = self.client.post(
+            '/api/users/login',
+            data=json.dumps(invalid_data),
+            content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertJSONEqual(
+            response.content,
+            {
+                "error": "Invalid credentials."
+            }
+        )
+
+    def test_login_fail_invalid_password(self):
+        invalid_data = {
+            "username": self.existing_user.username,
+            "password": "bad_password"
+        }
+
+        response = self.client.post(
+            '/api/users/login',
+            data=json.dumps(invalid_data),
+            content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertJSONEqual(
+            response.content,
+            {
+                "error": "Invalid credentials."
             }
         )
 
