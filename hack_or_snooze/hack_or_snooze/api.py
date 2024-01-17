1b7de890c057dca03035b6b26ab4fec0c86a3c6f
Brian Aston <astonb.dev@gmail.com>
2024-01-16 17:47:18 -0800

    Made some changes to throw custom error on malformed username on signup. TODO, adjust return value for this new error case, and fix test

--- hack_or_snooze/hack_or_snooze/api.py
+++ hack_or_snooze/hack_or_snooze/api.py
@@ -3,12 +3,22 @@ from ninja import NinjaAPI
 from stories.api import router as stories_router
 from users.api import router as users_router
 
+from users.exceptions import InvalidUsernameException
+
 api = NinjaAPI()
 
-# STAFFNOTE: it's possible to add middleware-style auth to all operations 
+# STAFFNOTE: it's possible to add middleware-style auth to all operations
 # declared below! investigate this further down the line:
+
 
 api.add_router("/users/", users_router)
 api.add_router("/stories/", stories_router)
 
 
+@api.exception_handler(InvalidUsernameException)
+def on_invalid_username(request, exc):
+    return api.create_response(
+        request,
+        {"detail": "Invalid username"},
+        status=401
+    )
---
+++ hack_or_snooze/users/exceptions.py
@@ -0,0 +1,2 @@
+class InvalidUsernameException(Exception):
+    pass
