0edc757e2e2a5db5c2e1ea134c061f753ff518b7
Brian Aston <astonb.dev@gmail.com>
2024-01-24 10:51:51 -0800

    Added title and description for the api. Includes high level HOW TO USE documentation

--- hack_or_snooze/hack_or_snooze/api.py
+++ hack_or_snooze/hack_or_snooze/api.py
@@ -5,8 +5,45 @@ from users.api import router as users_router
 
 from hack_or_snooze.exceptions import InvalidUsernameException
 
+description = """
+How to Use This API
+-------------------
 
-api = NinjaAPI()
+First, start by registering a new user using the **/api/users/signup** route
+sending the following information:
+
+    {
+        "username": "string",
+        "first_name": "string",
+        "last_name": "string",
+        "password": "string"
+    }
+
+Upon a successful request to that endpoint, you will receive a JSON payload that
+includes your user details as well as a token. Like so:
+
+    {
+        "token": "your_username:token_data"
+        "user": ...
+    }
+
+With your new token, you can now use all of the endpoints that require auth
+(the endpoints marked with a lock). You will need to send this token in the 
+HEADERS of your request like so:
+
+    HEADER: token: your_token
+
+You can also supply this token to the **AUTHORIZE** button located at the top of this
+documentation for usage in these interactive endpoints.
+
+"""
+
+
+api = NinjaAPI(
+    title="Hack Or Snooze API",
+    description=description
+    
+)
 
 
 api.add_router("/users/", users_router)
