a43861c64f68a6c6d38915967bf034a8e8afa754
Edmond Soun <edmond@rithmschool.com>
2024-08-06 16:11:58 -0700

    build script updates

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ pip install -r requirements.txt
 # python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+hack_or_snooze/python manage.py migrate
